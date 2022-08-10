4cacc4ba101e96d8153a1d3748cd87bd3939539f
Ezekiel Newren <ezekielnewren@gmail.com>
2022-08-10 17:45:05 -0600

    generate ovpn, unable to get local issuer certificate

---
+++ generate_ovpn.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+fqdn="$1"
+ttl="30d"
+ttl="24h"
+
+secret=$(vault write -format=json authority/pki/issue/ezekielnewren-dot-com common_name="$fqdn" ttl="$ttl" | jq -rMc .)
+
+echo "client"
+echo "nobind"
+echo "dev tun"
+echo "remote-cert-tls server"
+echo
+echo "remote vpn.ezekielnewren.com 1194 udp"
+echo
+echo "<key>"
+echo $secret | jq -rM .data.private_key
+echo "</key>"
+echo "<cert>"
+echo $secret | jq -rM .data.certificate
+echo $secret | jq -rM .data.ca_chain[]
+echo "</cert>"
+echo "<ca>"
+vault kv get service/secret/certificate | jq -rM .data.data.EzekielNewrenRoot
+echo "</ca>"
+echo "key-direction 1"
+echo "<tls-auth>"
+vault kv get service/secret/openvpn | jq -rM .data.data.ta
+echo "</tls-auth>"
+echo
+echo "redirect-gateway def1"
