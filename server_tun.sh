b4d912864551ec99e44c6e2506da261ab362a455
Ezekiel Newren <ezekielnewren@gmail.com>
2022-08-12 23:46:33 -0600

    use SHA256 for hmac

--- server_tun.sh
+++ server_tun.sh
@@ -8,4 +8,6 @@
 
 
 docker build -t openvpn . || exit 1
-docker run --name vpn_tun --privileged --restart always -itd -w /docker -v $PWD:/docker -v /etc/ssl:/etc/ssl --network=host openvpn
+docker run --name vpn_tun --privileged --restart always -itd \
+-w /docker -v $PWD:/docker -v /etc/ssl:/etc/ssl --network=host \
+openvpn
