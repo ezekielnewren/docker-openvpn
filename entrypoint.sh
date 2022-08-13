e93d48499bea3d40f6133ae67c40767827c97156
Ezekiel Newren <ezekielnewren@gmail.com>
2022-08-12 23:39:10 -0600

    put the chain under /etc/ssl/ca/chain.pem

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 mkdir -p /etc/ssl/issued 2>/dev/null
+mkdir -p /etc/ssl/ca 2>/dev/null
 mkdir -p /etc/ssl/private 2>/dev/null
 
 name="vpn.ezekielnewren.com"
-jq -rM .data.certificate /etc/ssl/vault/$name.json > /etc/ssl/issued/$name.crt
-jq -rM .data.ca_chain[] /etc/ssl/vault/$name.json >> /etc/ssl/issued/$name.crt
-jq -rM .data.private_key /etc/ssl/vault/$name.json > /etc/ssl/private/$name.key && chmod 640 /etc/ssl/private/$name.key
+file_json="/etc/ssl/private/vault/$name.json"
+jq -rM .data.certificate $file_json > /etc/ssl/issued/$name.crt
+jq -rM .data.ca_chain[] $file_json > /etc/ssl/ca/chain.pem
+cat /etc/ssl/certs/EzekielNewrenRoot.pem >> /etc/ssl/ca/chain.pem
+jq -rM .data.private_key $file_json > /etc/ssl/private/$name.key && chmod 640 /etc/ssl/private/$name.key
