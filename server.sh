df7ac7e288d1c3207cf7205aa85659c94cb178ee
Ezekiel Newren <ezekielnewren@gmail.com>
2022-08-28 06:42:53 +0000

    build image using the host network

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@
 # sudo iptables -t nat -I POSTROUTING -s 192.168.255.0/24 -o br0 -j MASQUERADE
 
 
-docker build -t openvpn . || exit 1
+docker build --network host -t openvpn . || exit 1
 docker rm -f vpn_home
 docker run --name vpn_home --privileged --restart always -itd \
 -w /docker -v $PWD:/docker -v /etc/ssl:/etc/ssl --network=host \
